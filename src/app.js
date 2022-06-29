7396ae6d8807d97149b7b66dc537cb40ff142723
Sussanara <sussanaracely@gmail.com>
2022-06-29 20:14:36 +0000

    Domain

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
-import "bootstrap";
 import "./style.css";
-import "./style.css";
-import "./index.html";
 import "./index.html";
 
 window.onload = function() {
   //write your code here	  //write your code here
 
+  let domainGenerator = [];
   let pronoun = ["El", "Su"];
   let adj = ["terrible", "rico"];
+  let noun = ["pollo", "chicken"];
+  let domain = [".com", ".cl"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < domain.length; d++) {
+          domainGenerator.push(" " + pronoun[p] + adj[a] + noun[n] + domain[d]);
+        }
+      }
+    }
+  }
+  console.log(domainGenerator);
+  document.getElementById("Domain.Name").innerHTML = domainGenerator;
 };
